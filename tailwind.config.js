4c1df5a38fdda2801fcb29c927421532bb4fff81
guijunzhang <gzhang89@asu.edu>
2023-06-11 20:12:58 -0700

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,11 +9,24 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        primary: '#E8EBE4', /* This color code represents a light shade. 
+                               It could be used as the background color 
+                               for your website or as a neutral color for 
+                               text and other elements. */
+        secondary: '#D2D5DD', /* This color code represents a medium-light shade.
+                                 It could be used as a secondary background color 
+                                 or for dividing sections on your website. */
+        accented1: '#B8BACF', /* This color code represents a medium shade. 
+                                 It could be used for headers, buttons, or 
+                                 other prominent elements to create visual interest. */
+        accented2: '#999AC6', /* This color code represents a medium-dark shade. 
+                                 It could be used for accents, such as links or hover effects, 
+                                 to draw attention to specific elements. */
+        contrast: '#798071', /* This color code represents a dark shade. 
+                                It could be used for text or footer elements 
+                                to provide contrast against lighter backgrounds. */
+      }
     },
   },
   plugins: [],
