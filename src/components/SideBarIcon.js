945514baa1bc2beace375de4e7a14f78338fd43e
guijunzhang <gzhang89@asu.edu>
2023-06-12 20:44:59 -0700

    color scheme changes + toggle mode changes

--- src/components/SideBarIcon.js
+++ src/components/SideBarIcon.js
@@ -4,19 +4,21 @@ export default function SideBarIcon(props) {
     return (
         <Link href={"/" + `${props.dir}`}>
             <div 
-                className="sidebar-icon group"
-                onClick={props.toggle}
-            >
+                className={
+                    "sidebar-icon group hover:bg-accented2 " + 
+                    (props.on ? "rounded-xl" : "")
+                }
+                onClick={props.toggle}>
                 
-                <div className={
-                        "pill group-hover:scale-100 " + 
-                        (props.on ? "scale-100 h-10" : "scale-0")
-                    }
-                >
+                <div 
+                className={
+                    "pill bg-accented2 group-hover:scale-100 " + 
+                    (props.on ? "scale-100 h-10" : "scale-0")
+                }>
                     
                 </div>
                 {props.icon}
-                <span className="sidebar-tooltip group-hover:scale-100">
+                <span className="sidebar-tooltip bg-accented2 group-hover:scale-100">
                     {props.text}
                 </span>
                 
